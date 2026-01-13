2eb9112a693b93089706a53f2b82b63063087ebc
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-13 17:32:54 +0530

    dependency injection, @Component

---
+++ SpringBoot/src/main/java/com/Kushagra/app/Application.java
@@ -0,0 +1,19 @@
+package com.Kushagra.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+		System.out.println("Hello world");
+
+		Human obj = context.getBean(Human.class);
+		obj.code();
+	}
+
+}
---
+++ SpringBoot/src/main/java/com/Kushagra/app/Human.java
@@ -0,0 +1,10 @@
+package com.Kushagra.app;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Human {
+    public void code(){
+        System.out.println("Coding");
+    }
+}
