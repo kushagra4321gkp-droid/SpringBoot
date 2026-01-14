c6d9db8045784c04f9d83490dc5000b28fa6dac2
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-14 22:10:05 +0530

    Autowiring --> without having the direct access to ApplicationContext

--- SpringBoot/src/main/java/com/Kushagra/app/Human.java
+++ SpringBoot/src/main/java/com/Kushagra/app/Human.java
@@ -1,10 +1,17 @@
 package com.Kushagra.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Human {
+
+    @Autowired
+    laptop lap;
+
     public void code(){
-        System.out.println("Coding");
+//        System.out.println("Coding");
+
+        lap.compile();
     }
 }
---
+++ SpringBoot/src/main/java/com/Kushagra/app/laptop.java
@@ -0,0 +1,11 @@
+package com.Kushagra.app;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class laptop {
+
+    public void compile(){
+        System.out.println("Compiling...");
+    }
+}
